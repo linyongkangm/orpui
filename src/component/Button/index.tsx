82a496e86237a34b392d50976716708b347441a4
linyongkangm <linyongkangm@foxmail.com>
2019-03-12 21:20:40 +0800

    feat:button loading

--- src/component/Button/index.tsx
+++ src/component/Button/index.tsx
@@ -1,7 +1,7 @@
 import { withPredefined, prefixTo } from '$widget/withPredefined';
 import * as React from 'react';
 import Icon from '$component/Icon';
-import { addClassName as aCN } from '$tools';
+import { addClassName as aCN, isString } from '$tools';
 
 import './style.scss';
 export enum Design {
@@ -27,7 +27,6 @@ export interface IProps extends React.HTMLAttributes<HTMLButtonElement> {
   disabled?: boolean;
   block?: boolean;
   loading?: boolean;
-
   icon?: string;
 }
 
@@ -47,16 +46,25 @@ function Button(props: IProps = {}) {
     ...others
   } = props;
 
-
   let realClassName = className;
   if (size) realClassName = aCN(realClassName, prefixTo(`button__${size}`));
   if (shape) realClassName = aCN(realClassName, prefixTo(`button__${shape}`));
   if (design) realClassName = aCN(realClassName, prefixTo(`button__${design}`));
   if (disabled) realClassName = aCN(realClassName, prefixTo(`button__disabled`));
   if (block) realClassName = aCN(realClassName, prefixTo(`button__block`));
-  if (loading) realClassName = aCN(realClassName, prefixTo(`button__loading`));
+  console.log();
   return (
-    <Icon design='circle-o-notch' spin={true}></Icon>
+    <button
+      className={realClassName}
+      onClick={(e) => !disabled && !loading && onClick && onClick(e)}
+      {...others}>
+      {
+        loading ? <Icon design='circle-o-notch' spin={true}></Icon> : ''
+      }
+      {
+        isString(children) ? <span>{children}</span> : children
+      }
+    </button>
   );
 }
 
